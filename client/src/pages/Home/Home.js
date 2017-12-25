ba726997553a89f22c0d6416e7426a61cde5a796
jinredhub <jin@nieblo.com>
2017-12-25 15:06:11 -0800

    changed array name. added add button to add furniture

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -9,6 +9,7 @@ import Rnd from "react-rnd-rotate"
 import {database} from "./firebase";
 
 
+
 class Home extends Component {
 
   state = {
@@ -17,9 +18,7 @@ class Home extends Component {
       {arrayId: 1, buttonId: "button1", id: "Sauder Desk", degree: 0, position: {x: 50, y: 50}, size: {width: 100, height: 100}, image: "/images/sauderDesk.png", imageTop: "/images/sauderDeskTop.png"},
     ],
   	modelsData: [
-      {id: "roomRatio", ratio: {x: 500, y: 300}},
-      {arrayId: 0, id: "Mayline Chair", degree: 0, position: {x: 10, y: 10}, size: {width: 50, height: 50}, image: "/images/maylineChair.png", imageTop: "/images/maylineChairTop.png"},
-      {arrayId: 1, id: "Sauder Desk", degree: 0, position: {x: 50, y: 50}, size: {width: 100, height: 100}, image: "/images/sauderDesk.png", imageTop: "/images/sauderDeskTop.png"},
+      {arrayId: 0, id: "roomRatio", degree: 0, position: {x: 0, y: 0}, size: {width: 500, height: 300}, imageTop: "/images/floor.jpg"},
   	],
   };
 
@@ -64,62 +63,53 @@ class Home extends Component {
     		<Row>
 	        	<Col size = "col-md-10 order-md-2">
 	        		<div id="arrange-room">
-                <Rnd
-                  className="room"
-                  resizeGrid={[10,10]}
-                  default={{
-                    width: this.state.modelsData[0].ratio.x,
-                    height: this.state.modelsData[0].ratio.y,
-                  }}
-                  onResize={(e,direction,ref,delta,position)=>{
-                    let modelsData = Object.assign({}, this.state.modelsData);
-                    modelsData[0].ratio.x = ref.offsetWidth;
-                    modelsData[0].ratio.y = ref.offsetHeight;
-                    this.setState({
-                      modelsData
+                {this.state.modelsData.map((value,i)=>{
+                  return (
+                    value.id === "roomRatio" ?
+                     (
+                   <Rnd
+                    key={value.arrayId}
+                    className="room"
+                    resizeGrid={[10,10]}
+                    default={{
+                      width: value.size.width,
+                      height: value.size.height,
+                    }}
+                    onResize={(e,direction,ref,delta,position)=>{
+                      let modelsData = Object.assign({}, this.state.modelsData);
+                      modelsData[i].size.width = ref.offsetWidth;
+                      modelsData[i].size.height = ref.offsetHeight;
+                      console.log("in map modelsData: ", modelsData);
+                      this.setState({
+                        modelsData: this.state.modelsData
+                      })}}
+                  >
+                  </Rnd>
+                      ): (
+                  <Rnd
+                    key={value.arrayId}
+                    dragGrid={[10,10]}
+                    default={{
+                      x: value.position.x,
+                      y: value.position.y,
+                      width: value.size.width,
+                      height: value.size.height,
+                      degree: value.degree
+                    }}
+                    onDragStop={(e,d)=>{
+                      let modelsData = Object.assign({}, this.state.modelsData);
+                      modelsData[i].position.x = d.x;
+                      modelsData[i].position.y = d.y;
+                      modelsData[i].degree = d.degree;
+                      this.setState({
+                        modelsData: this.state.modelsData
                     })}}
-                >
-                </Rnd>
-                <Rnd
-                  dragGrid={[10,10]}
-                  default={{
-                    x: this.state.modelsData[1].position.x,
-                    y: this.state.modelsData[1].position.y,
-                    width: this.state.modelsData[1].size.width,
-                    height: this.state.modelsData[1].size.height,
-                    degree: this.state.modelsData[1].degree
-                  }}
-                  onDragStop={(e,d)=>{
-                    let modelsData = Object.assign({}, this.state.modelsData);
-                    modelsData[1].position.x = d.x;
-                    modelsData[1].position.y = d.y;
-                    modelsData[1].degree = d.degree;
-                    this.setState({
-                      modelsData
-                  })}}
-                >
-                  <img className="img-fluid" src={this.state.modelsData[1].imageTop} draggable="false" />
-                </Rnd>
-                <Rnd
-                  dragGrid={[10,10]}
-                  default={{
-                    x: this.state.modelsData[2].position.x,
-                    y: this.state.modelsData[2].position.y,
-                    width: this.state.modelsData[2].size.width,
-                    height: this.state.modelsData[2].size.height,
-                    degree: this.state.modelsData[2].degree
-                  }}
-                  onDragStop={(e,d)=>{
-                    let modelsData = Object.assign({}, this.state.modelsData);
-                    modelsData[2].position.x = d.x;
-                    modelsData[2].position.y = d.y;
-                    modelsData[2].degree = d.degree;
-                    this.setState({
-                      modelsData
-                  })}}
-                >
-                  <img className="img-fluid" src={this.state.modelsData[2].imageTop} draggable="false" />
-                </Rnd>
+                  >
+                    <img className="img-fluid" alt={value.id} src={value.imageTop} draggable="false" />
+                  </Rnd>
+                      ));
+
+                })}
 	        		</div>
 	        	</Col>
 
