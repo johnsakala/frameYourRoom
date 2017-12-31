90b925f766a42d19f7c07f6f139566d0b25ad120
jinredhub <jin@nieblo.com>
2017-12-30 20:12:22 -0800

    added more furniture

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -14,8 +14,15 @@ class Home extends Component {
 
   state = {
     allData: [
-      {arrayId: 1, buttonId: "button1", id: "Mayline Chair", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "55 W, 55 D, 55 H cm", degree: 0, position: {x: 10, y: 10}, size: {width: 50, height: 50}, image: "/images/maylineChair.png", imageTop: "/images/maylineChairTop.png"},
-      {arrayId: 2, buttonId: "button2", id: "Sauder Desk", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "55 W, 55 D, 55 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 100, height: 50}, image: "/images/sauderDesk.png", imageTop: "/images/sauderDeskTop.png"},
+      {arrayId: 1, buttonId: "button1", id: "Mayline Chair", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "74 W, 74 D, 110 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 74, height: 74}, image: "/images/maylineChair_prod.png", imageTop: "/images/maylineChair_Top.png"},
+      {arrayId: 2, buttonId: "button2", id: "Sauder Desk", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "150 W, 65 D, 110 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 150, height: 65}, image: "/images/sauderDesk_prod.png", imageTop: "/images/sauderDeskTop.png"},
+      {arrayId: 3, buttonId: "button3", id: "BIL Bookcase", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "80 W, 28 D, 106 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 80, height: 28}, image: "/images/BIL_bookcase_prod.jpg", imageTop: "/images/BIL_Bookcase_Top.png"},
+      {arrayId: 4, buttonId: "button4", id: "KAL Shelf unit", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "86 W, 39 D, 147 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 86, height: 39}, image: "/images/KAL_Shelf_unit_prod.jpg", imageTop: "/images/KAL_Shelf_unit_top.png"},
+      {arrayId: 5, buttonId: "button5", id: "POT Uplighter", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "28 W, 28 D, 174 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 28, height: 28}, image: "/images/POT_uplighter_prod.jpg", imageTop: "/images/POT_uplighter_top.png"},
+      {arrayId: 6, buttonId: "button6", id: "MOTO Uplighter", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "28 W, 28 D, 174 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 28, height: 28}, image: "/images/MOTO_uplighter_prod.jpg", imageTop: "/images/MOTO_uplighter_top.png"},
+      {arrayId: 7, buttonId: "button7", id: "KARLS_Sofa", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "205 W, 93 D, 80 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 205, height: 93}, image: "/images/KARLS_Sofa_prod.jpg", imageTop: "/images/KARLS_Sofa_top.png"},
+      {arrayId: 8, buttonId: "button8", id: "VIM_Sofa", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "241 W, 98 D, 80 H cm", degree: 0, position: {x: 50, y: 50}, size: {width: 241, height: 98}, image: "/images/VIM_Sofa_prod.png", imageTop: "/images/VIM_Sofa_top.png"},
+      {arrayId: 9, buttonId: "button9", id: "Picture A", className: "btn btn-primary", buttonText: "Add to scene", dimensions: "200 W, 140 D cm", degree: 0, position: {x: 50, y: 50}, size: {width: 200, height: 20}, image: "/images/picture_A_prod.png", imageTop: "/images/picture_A_top.png"},
     ],
   	modelsData: [
       {arrayId: 0, id: "roomRatio", degree: 0, position: {x: 0, y: 0}, size: {width: 500, height: 300}, imageTop: "/images/floor.jpg"},
